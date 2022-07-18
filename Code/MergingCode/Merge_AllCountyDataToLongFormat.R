ae57453ad3b209a732b3de444b126ab228b316ba
CoryRoth <cgroth@iastate.edu>
2022-07-18 10:46:18 -0500

    Update Merge_AllCountyDataToLongFormat.R

--- Code/MergingCode/Merge_AllCountyDataToLongFormat.R
+++ Code/MergingCode/Merge_AllCountyDataToLongFormat.R
@@ -2,15 +2,16 @@ library(readxl)
 library(tidycensus)
 library(tidyverse)
 library(readr)
-library(shiny)
 
-
+##Takes in All Data
 Overall <- read_csv("Data\\AllCountyData\\OverallDatabase.csv", col_names  = TRUE)
 
 
-OVerallT<- as.data.frame(t(Overall))
+##Converts To Long format
+##Keeps Fips, County name in wide
 Overall2<- Overall%>%
   pivot_longer(3:ncol(Overall))
 
+##Saves to File
 write.csv(Overall2,"Data\\AllCountyData\\OverallDatabaseLong.csv",row.names = FALSE)
 
