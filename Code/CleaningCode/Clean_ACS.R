e84ccc51a84b3ab204ae21bd1ee92c3b0034bb6d
CoryRoth <cgroth@iastate.edu>
2022-07-20 10:29:11 -0500

    Cleaned ACS Data

--- Code/CleaningCode/Clean_ACS.R
+++ Code/CleaningCode/Clean_ACS.R
@@ -5,7 +5,36 @@ library(readr)
 
 Data <-read.csv("Data\\RawData\\ACS\\RAW_ACS.csv")
 
+DATA2<- Data[,3:ncol(Data)]
+MOE <- DATA2[,endsWith(colnames(DATA2),"M")]
+MOE[,"GEOID"] <- Data$GEOID
+MOE[,"NAME"] <- Data$NAME
 
+MOE<-MOE%>%
+  select(GEOID,NAME,everything())
+
+ACSDATA<- DATA2[,!endsWith(colnames(DATA2),"M")]
+ACSColNames <- colnames(ACSDATA)
+ACSColNames <- substr(ACSColNames,0,nchar(ACSColNames)-1)
+
+colnames(ACSDATA)<- ACSColNames
+ACSDATA[,"GEOID"] <- MOE$GEOID
+ACSDATA[,"NAME"] <- MOE$NAME
+
+ACSDATA<-ACSDATA%>%
+  select(GEOID,NAME,everything()) 
+
+
+ACSDATA<-ACSDATA%>%
+  group_by(NAME)%>%
+  mutate(AffordablityIndex = MedianValueOwner/MedianEarnings, HousingPriceDispersion = UpperQuartileValue/LowerQuartileValue, CostBurdenOwnver=  sum(OwnerCost30_35PIncome,OwnerCostOver35PIncome),
+         CostBurdenRenter = sum(RenterCostOver35PIncome,RenterCost30_35PIncome),MutliFamShare = sum(MultiFam2Units,MultiFam20Units,MultiFam5_9Units,MUltiFam3_4Units,MultiFam10_19Units),
+         RentalUnitRatio  =sum(ForRent,RenterOCCUPIED,RenderNotOccupied)/OccupiedHousingUnits)
+FinalAcs <- ACSDATA %>%
+  select(1:2,TypicalRenterCosts = GrossRent, TypicalOwnerCosts= MedianMontlyCostOwner, AgingHousingStock = PercentHouseholdsBuildPre1940,AffordabilityIndex = AffordablityIndex,HousingPriceDispersion,CostBurdenOwner  =CostBurdenOwnver,CostBurdenRenter, HomeOwnerShipRate,RentalUnitRatio,MultiFamShare=MutliFamShare)
+
+
+write.csv(FinalAcs,"Data\\CleanData\\Ready_ACS.csv")
 
 ##AFFORABLILITY INDEX  = MEDIAN VALUE / MEDIAN EARNINGS
 ##HOUSING PRICE DISPERSION = UPPERQUARTILE/LOWERQUARTILE
@@ -13,4 +42,6 @@ Data <-read.csv("Data\\RawData\\ACS\\RAW_ACS.csv")
 ##Cost Burdened Renter = SUM RenterCostsPercent...  MOE = root(moe^2 + moe^2)
 ##MULTI Fam = Sum MultFamUnits...MOE = Root(moe^2+moe^2+moe^2...)
 ##RentalUnitRatio = SUm forRent, RenterNot Occupied, Renter Occupied, moe
-##Ratio  total rental units by total housing units
+  ##Ratio  total rental units by total housing units
+
+  
